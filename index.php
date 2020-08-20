3f831cb65a1cdf6f87e5c91c897dca7eb54a3b2d
Alireza Akrami <akrami.ui@gmail.com>
2020-08-20 19:53:36 +0200

    route using klein

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Klein\Klein;
+
+$router = new Klein();
+
+$router->respond('GET', '/', function ($request, $response) {
+    $response->code(200);
+    $response->json([
+        'status' => 'OK',
+        'message' => 'welcome to sudoku solver'
+    ]);
+});
+
+$router->dispatch();
