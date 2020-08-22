239ebe6623691f89cfcef636d35edd0ba1677eae
Alireza Akrami <akrami.ui@gmail.com>
2020-08-23 00:45:55 +0200

    web interface

--- index.php
+++ index.php
@@ -2,27 +2,34 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use App\Service\Sudoku;
+use Klein\App;
 use Klein\Klein;
 use Klein\Request;
 use Klein\Response;
+use Klein\ServiceProvider;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 $router = new Klein();
+
+$router->respond(function (Request $request, Response $response, ServiceProvider $service, App $app) use ($router) {
+    $app->register('twig', function () {
+        $loader = new FilesystemLoader("templates");
+        return new Environment($loader, ["debug" => true]);
+    });
+});
 
 /*
  * Welcome request. nothing important
  */
-$router->respond('GET', '/', function ($request, $response) {
-    $response->code(200);
-    $response->json([
-        'status' => 'OK',
-        'message' => 'welcome to sudoku solver'
-    ]);
+$router->respond('GET', '/', function (Request $request, Response $response, ServiceProvider $service, App $app) {
+    return $app->twig->render('home.html.twig', ["name" => "Shalgham"]);
 });
 
 /**
  * Solve a sudoku
  */
-$router->respond('POST', '/solve', function (Request $request, Response $response) {
+$router->respond('POST', '/api/solve', function (Request $request, Response $response) {
     if (preg_match('/application\/json/', $request->headers()->get('Content-Type'))) {
         $body = json_decode($request->body());
         if (isset($body->grid)) {
